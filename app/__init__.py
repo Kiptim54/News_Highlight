ba59693b21f3ac1c88e68ad1807b2593218d5ee1
Kiptim <kiptim54@gmail.com>
2018-04-07 15:00:11 +0300

    initial file setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,6 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from config import config_options
+ 
+#initialize the app
+app = Flask(__name__)
 
-bootstrap = Bootstrap()
-
-def create_app(config_name):
-    app = Flask(__name__)
-
-    #app configurations
-    app.config.from_object(config_options[config_name])
-
-    #initialize extensions
-    bootstrap.init.app(app)
-
-
-
-
-
-
+from app import views

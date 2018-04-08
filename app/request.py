c436316c2b7e24f8990e43e01b60279de7445582
Kiptim <kiptim54@gmail.com>
2018-04-08 14:20:50 +0300

    add if statement

--- app/request.py
+++ app/request.py
@@ -1,42 +1,56 @@
 from app import app
-import urllib.request, json
-from newsapi import NewsApiClient
+import urllib.request,json
 from .models import newssource
-Newssource=newssource.Newssource
-#Getting api key
+from newsapi import NewsApiClient
+
+
+Newssource = newssource.Newssource
+
+# access the api  key
 api_key = app.config['NEWS_API_KEY']
-#Getting the movie base url
-base_url = app.config['NEWS_API_BASE_URL']
-def get_sources(category):
+
+# news base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_sources(source):
     '''
-    Function that gets the json response to our url request
+    Function gets json response
     '''
-    get_sources_url = base_url + api_key
-    with urllib.request.urlopen(get_sources_url) as url:
-        get_sources_data = url.read()
-        get_sources_response = json.loads(get_sources_data)
-        sources_results= None
-        if get_sources_response['sources']:
-            sources_results_list = get_sources_response['sources']
-            sources_results = process_results(sources_results_list)
-    return sources_results
-def process_results(sources_list):
+    get_source_url = base_url + api_key
+
+    with urllib.request.urlopen(get_source_url) as url:
+        get_sources_data  = url.read()
+        get_source_dict = json.loads(get_sources_data)
+        
+        source_results = None
+        if get_source_dict['sources']:
+            source_results_list = get_source_dict['sources']
+            source_results = process_results(source_results_list)
+    return source_results
+    
+
+ 
+def process_results(source_list):
     '''
-    Function that process the movie result and transforms them to a list of Objects
-    Args:
-        sources_list: A list of dictionaries that contain news source details
-    Returns :
-        sources_results: A list of news sources
+    Function  that processes the movie result and transform them to a list of Objects
     '''
-    sources_results = []
-    for sources_item in sources_list:
-        id = sources_item.get('id')
-        name = sources_item.get('name')
-        description = sources_item.get('description')
-        url= sources_item.get('url')
+    
+    source_results = []
+
+    for source_item in source_list:
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+
         if url:
-            sources_object = Newssource(id,name,description,url)
-            sources_results.append(sources_object)
-            print(sources_results)
-    return sources_results
-   
+            source_object = Newssource(id,name,description,url)
+            source_results.append(source_object)
+    print(source_results)
+    return source_results
+    
+
+
+    
+
+
+        
--- app/views.py
+++ app/views.py
@@ -1,15 +1,16 @@
-from flask import render_template
 from app import app
 from .request import get_sources
-#Views
+from flask import render_template
+
+
 @app.route('/')
 def index():
     '''
-    View root page function that returns the index page and its data
+    function returns the index page
     '''
-    status_sources = get_sources('status')
+    # newssource
+    news_source = get_sources('status')
     title= "Kiptim's New's Highlight"
-    return render_template('index.html', title = title, status = status_sources)
-
-
-
+    
+    
+    return render_template('index.html', title=title, sources=news_source)
